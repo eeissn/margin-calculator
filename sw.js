93deba9fb85a5a784c28869c86d5cbea24ad44f7
eeissn <eisenpfad@gmail.com>
2026-09-16 23:52:50 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'margin-calculator-v1';
+
+const APP_FILES = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
